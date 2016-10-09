c18fbf0f0d7ba87ff276cd29d451b17e52aa1608
andrey-tkachuk94 <andrii.tkachuk94@gmail.com>
2016-10-09 15:51:43 +0300

    Update login.php

--- kohana/application/views/admin/login.php
+++ kohana/application/views/admin/login.php
@@ -1,13 +1,26 @@
 <div style="max-width: 300px; margin: 0 auto;">
+    
+    <style>
+        .btn-default {
+            background-color: grey;
+            color: white;
+        }
+        h2 {
+            text-align: center;
+        }
+        #submit {
+            text-align: center;
+        }
+    </style>
 
-<h2>Log in</h2>
+<h2>Sign in</h2>
 
 <?php if ($form['state'] == 'errors'): ?>
     <div class="alert alert-danger">
         <?php if ($form['global_error']): ?>
         <?php echo $form['global_error'] ?>
         <?php else: ?>
-        Wpisz login oraz hasło
+        Incorrect login and/or password!
         <?php endif; ?>
     </div>
 <?php endif; ?>
@@ -16,13 +29,15 @@
     <input type="hidden" name="login_form" value="1">
   <div class="form-group<?php echo !empty($form['errors']['username']) ? ' has-error' : '' ?>">
     <label for="exampleInputUsername1">Username</label>
-    <input type="text" name="username" class="form-control" id="exampleInputUsername1" placeholder="Enter username" value="<?php echo !empty($form['data']['username']) ? $form['data']['username'] : '' ?>">
+    <input type="text" name="username" required class="form-control" id="exampleInputUsername1" placeholder="Enter username" value="<?php echo !empty($form['data']['username']) ? $form['data']['username'] : '' ?>">
   </div>
   <div class="form-group<?php echo !empty($form['errors']['password']) ? ' has-error' : '' ?>">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" name="password" required class="form-control" id="exampleInputPassword1" placeholder="Enter password">
   </div>
-  <button type="submit" class="btn btn-default">Submit</button>
+    <div id="submit">
+  <button type="submit" class="btn btn-default">Login</button>
+    </div>
 </form>
 
 </div>
