73fab6dceac29ec0aee9ff5c465585024d967173
andrey-tkachuk94 <andrii.tkachuk94@gmail.com>
2016-10-09 15:48:22 +0300

    Update admin.php

--- kohana/application/views/admin.php
+++ kohana/application/views/admin.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Admin</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="<?php echo URL::base() ?>admin/css/bootstrap.min.css" rel="stylesheet" media="screen">
-  </head>
-  <body>
-    
-    <?php echo $content ?>
+<head>
+    <title>Admin panel</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-    <script src="//code.jquery.com/jquery.js"></script>
-    <script src="<?php echo URL::base() ?>admin/js/bootstrap.min.js"></script>
-  </body>
+<?php echo $content ?>
+
+</body>
 </html>
